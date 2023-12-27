d4ccae1e20fbbc973657645d01bf1b21dc00c0a9
Henrique Varela <henrique11varela@gmail.com>
2023-12-27 21:58:13 +0000

    added guarded

--- src/app/Models/Application.php
+++ src/app/Models/Application.php
@@ -14,7 +14,7 @@ class Application extends Model
 
     use SoftDeletes;
 
-    protected $guarded = [];
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
 
     public function courses(): BelongsToMany
     {
