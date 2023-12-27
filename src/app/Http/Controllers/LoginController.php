156d5ed278cc649c17ffc30363fdaba4a7f98ee2
Pedro Pereira <pedroffpereira2001@gmail.com>
2023-12-27 23:14:58 +0000

    config sanctum login system

---
+++ src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Auth;
+
+class LoginController extends Controller
+{
+    function login(LoginRequest $request)
+    {
+        try {
+            $user = User::Where("email", $request->email)->first();
+            if (!$user) {
+                throw ValidationException::withMessages(["email" => "email invalid"]);
+            }
+            if (!Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages(["password" => "password invalid"]);
+            }
+            $user->tokens()->delete();
+            $token = $user->createToken($request->device_name)->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        } catch (ValidationException $e) {
+            return response()->json($e, 500);
+        }
+    }
+
+    function logout(Request $request)
+    {
+
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,9 +19,10 @@ class DatabaseSeeder extends Seeder
         ]);
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password'=> Hash::make('a'),
+        ]);
     }
 }
