26d835e53e2f67376cd886ed19bfd528b4ee7dc3
Pedro Pereira <pedroffpereira2001@gmail.com>
2024-01-04 21:40:13 +0000

    user api router

--- src/routes/api.php
+++ src/routes/api.php
@@ -117,6 +117,13 @@ Route::prefix('v1')->group(function () {
             Route::delete('{student}', [StudentController::class, 'destroy']);
         });
 
+        Route::prefix('users')->group(function () {
+            Route::get('', [UserController::class, 'index']);
+            Route::post('', [UserController::class, 'store']);
+            Route::get('{user}', [UserController::class, 'show']);
+            Route::put('{user}', [UserController::class, 'update']);
+            Route::delete('{user}', [UserController::class, 'destroy']);
+        });
 
     });
 });
