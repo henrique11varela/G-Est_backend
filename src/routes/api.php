242f4852bd2794f38d85d495e59e16834df99701
Henrique Varela <henrique11varela@gmail.com>
2024-01-05 02:19:16 +0000

    setup ports and disabled login system 4 now

--- src/routes/api.php
+++ src/routes/api.php
@@ -34,7 +34,7 @@ Route::prefix('v1')->group(function () {
 
     Route::post('login', [LoginController::class, 'login']);
 
-    Route::group(['middleware' => 'auth:sanctum'], function () {
+    // Route::group(['middleware' => 'auth:sanctum'], function () {
 
 
         Route::prefix('applications')->group(function () {
@@ -118,5 +118,5 @@ Route::prefix('v1')->group(function () {
         });
 
 
-    });
+    // });
 });
