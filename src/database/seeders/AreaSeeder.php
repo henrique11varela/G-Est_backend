844c4fe61c562b7b58a0812fd09f8c934eb8c02b
Vasco Silva <vasco.silva.t0123187@edu.atec.pt>
2024-02-23 23:52:31 +0000

    Added final Area, coordinator and course seeders

--- src/database/seeders/AreaSeeder.php
+++ src/database/seeders/AreaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AreaSeeder extends Seeder
 {
@@ -12,7 +13,37 @@ class AreaSeeder extends Seeder
      */
     public function run(): void
     {
-
-        \App\Models\Area::factory(10)->create();
+        DB::table('areas')->insert([
+            [
+                'area_code' => 481,
+                'name' => 'Ciências Informáticas',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'area_code' => 521,
+                'name' => 'Metalurgia e Metalomecânica',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'area_code' => 522,
+                'name' => 'Eletrónica e Automação',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'area_code' => 523,
+                'name' => 'Eletricidade e Energia',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'area_code' => 525,
+                'name' => 'Construção e Reparação de Veículos a Motor',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
